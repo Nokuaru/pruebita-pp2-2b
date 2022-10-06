00e3d445df3565eb1de4705353d8a2c854157b03
Facundo Tomas <nokuaru@gmail.com>
2022-10-06 19:55:18 -0300

    Eliminado app.ts, creado intermedio, modificado index2

--- app.ts
+++
@@ -1,8 +0,0 @@
-let email: string = 'nokuaru@gmail.com';
-let website: string = 'https://www.recetasgratis.net/';
-
-
-let footer = document.createElement('h5');
-footer.textContent = `Email: ${email}` + ` Website: ${website}`
-
-document.body.appendChild(footer); 
